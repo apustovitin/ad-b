b0526c77681b63c7051b617e9434577b18ffcc17
apustovitin <apustovitin@gmail.com>
2021-05-23 21:54:10 +0300

    D16 key delete

--- ad_board/ad_board/settings.py
+++ ad_board/ad_board/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d9asgkg!re4o$c7^mf=e5+w!nxyme#-e8)0!oc3_r=qwa#%bkp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
